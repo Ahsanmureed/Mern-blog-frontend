e270cc89982f2280f1a36bc1b79acf06f7e0c5b3
Ahsanmureed <ahsanmureed00@gmail.com>
2024-03-28 20:42:54 +0500

    commit

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -67,10 +67,7 @@ const CreatePost = () => {
       let formData  = new FormData();
       formData.append("image",file);
       try {
-       const {data}= await axios.post("https://mern-blog-backend-chi-gray.vercel.app/api/v1/blog/image-upload", {
-         withCredentials: true,
-        
-       },formData)
+       const {data}= await axios.post("https://mern-blog-backend-chi-gray.vercel.app/api/v1/blog/image-upload",formData)
       } catch (error) {
        
       }
