ac3c49119c68525d328879a60c789320caca4ca8
Ahsanmureed <ahsanmureed00@gmail.com>
2024-06-03 15:58:18 +0500

    commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,9 +5,9 @@ const Post = ({blogs}) => {
 
   return (
     <div className=' flex gap-4 mb-3 mt-7 px-[3px] md:px-7 border-2 p-4 rounded-md  '>
-      <img className= ' w-[33%] md:w-[25%] md:h-[5%]   object-cover ' src={blogs.photo} alt="" />
+      <img className= ' w-[45%] md:w-[25%] h-[35vh]  md:h-[40vh]    ' src={blogs.photo} alt="" />
       <div>
-        <h1 className='text-2xl mb-2  font-bold'>{blogs.title} </h1>
+        <h1 className='text-2xl mb-2  font-bold'>{blogs?.title.length >50 ? blogs?.title.substring(0, 50) + '..' :blogs?.title} </h1>
         <div className='flex items-center  justify-between space-x-8 md:space-x-80'>
         <h1 className=' font-semibold mb-2'>@{blogs?.userId?.username}</h1>
         <div className='flex mb-2 gap-4'>
@@ -15,7 +15,7 @@ const Post = ({blogs}) => {
        <p className='hidden md:block'>{new Date(blogs.createdAt).toString().slice(16,24)}</p>
         </div>
         </div>
-        <p className=' w-[100%]'>{blogs.description.slice(0,200)+" ...Read more"} </p>
+        <p className=' w-[100%]'>{blogs?.description.length >50 ? blogs?.description.substring(0, 50) + '..' :blogs?.description} </p>
       </div>
     </div>
   )
