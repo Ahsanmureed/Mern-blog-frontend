0becddc5f286aeaf10960cfb168f26ca5fb4187c
Ahsanmureed <ahsanmureed00@gmail.com>
2024-06-13 18:33:59 +0500

    commit

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -5,7 +5,7 @@ import { UserContext } from "../context/UserContext"
 import LoaderLoader from "./LoaderLoader";
 export default function PrivateRoute({children}) {
  const {user}= useContext(UserContext)
-console.log(user);
+
  
     
   return user !== null? children : <div className=' h-[70vh] flex justify-center items-center'><LoaderLoader/></div>
